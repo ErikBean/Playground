4dfaa798bb2c5059b51b6c1229b40fcb4b210e0b
Legumebo Magezfeld <erikbean@lclark.edu>
2013-05-14 18:43:23 -0400

    initial commit from cloud9

---
+++ PlayGround/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayGround::Application.config.secret_token = 'f31a902967bb0fd9d4420de922218ec48775b2f440342dd2d0c560a82fe82d0a10e92e39a0c13c68fade4ec7c9215047d1614658819d5c31e0600ace1339defa'
